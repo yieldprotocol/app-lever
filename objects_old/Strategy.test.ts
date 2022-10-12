9c0d2bf4f30ea7c47ea5ece05865df6ffbd7b6be
brucedonovan <bruce.donovan@gmail.com>
2022-10-12 18:33:17 +0100

    bugfix/ts ignore stableSwap contract

--- objects_old/Strategy.test.ts
+++
@@ -1,7 +0,0 @@
-import { getInvestToken, STRATEGIES, StrategyName, Token } from "./Strategy";
-
-describe("getInvestToken", () => {
-  it("should return the correct token", () => {
-    expect(getInvestToken(STRATEGIES[StrategyName.WStEth])).toBe(Token.FyWeth);
-  });
-});
