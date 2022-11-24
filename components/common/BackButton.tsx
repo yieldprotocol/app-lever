541f42c9fbc0c9aa2c44e17b0c54ef8dc768129c
brucedonovan <bruce.donovan@gmail.com>
2022-11-24 11:56:26 +0000

    primary color tweak

--- components/common/BackButton.tsx
+++ components/common/BackButton.tsx
@@ -2,7 +2,7 @@ import { ArrowLeftIcon } from '@heroicons/react/24/solid';
 
 const BackButton = (props: any) => (
   <ArrowLeftIcon
-    className="my-auto h-5 w-5 hover:text-primary-400 hover:cursor-pointer"
+    className="my-auto h-5 w-5 hover:text-primary-600 hover:cursor-pointer"
     onClick={props.onClick}
     {...props}
   />
