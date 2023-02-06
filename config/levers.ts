c7abe0dbd74da1442e3dffcc782f9b106dbd88a0
brucedonovan <bruce.donovan@gmail.com>
2023-02-06 18:53:28 +0000

    add in all the notional assets

--- config/levers.ts
+++ config/levers.ts
@@ -1,6 +1,6 @@
 import { Simulator } from '../hooks/useLever';
 import { TokenType } from '../lib/types';
-import { DAI, FUSDC2303, USDC, WETH, WSTETH, YSDAI6MJD, YSETH6MJD, YSUSDC6MJD,  } from './assets';
+import { DAI, FDAI2303, FDAI2306, FETH2303, FETH2306, FUSDC2303, FUSDC2306, USDC, WETH, WSTETH, YSDAI6MJD, YSETH6MJD, YSUSDC6MJD,  } from './assets';
 
 
 import { STETH_LEVER, YIELD_STRATEGY_LEVER, NOTIONAL_LEVER } from './contracts';
@@ -88,6 +88,29 @@ export const LEVERS = new Map<string, ILeverRoot>();
 //   baseId: WETH,
 // });
 
+LEVERS.set('FETH_2303', {
+  ...notionalLeverCommon_,
+  id: 'FETH_2303',
+  displayName: 'FETH_2303 <> WETH',
+  maturity: 1680274800,
+  fyTokenAddress: '0x0FBd5ca8eE61ec921B3F61B707f1D7D64456d2d1',
+  seriesId: '0x303030390000',
+  ilkId: FETH2303,
+  baseId: WETH,
+});
+
+LEVERS.set('FDAI_2303', {
+  ...notionalLeverCommon_,
+  id: 'FDAI_2303',
+  displayName: 'FDAI_2303 <> DAI',
+  maturity: 1680274800,
+  fyTokenAddress: '0x79A6Be1Ae54153AA6Fc7e4795272c63F63B2a6DC',
+  seriesId: '0x303130390000',
+  ilkId: FDAI2303,
+  baseId: DAI,
+});
+
+
 LEVERS.set('FUSDC_2303', {
   ...notionalLeverCommon_,
   id: 'FUSDC_2303',
@@ -99,17 +122,38 @@ LEVERS.set('FUSDC_2303', {
   baseId: USDC,
 });
 
-// LEVERS.set('FUSDC_2306', {
-//   ...notionalLeverCommon_,
-//   id: 'FUSDC_2306',
-//   displayName: 'FUSDC_2306 <> USDC',
-//   maturity: 1680274800,
-//   fyTokenAddress: '0x22E1e5337C5BA769e98d732518b2128dE14b553C',
-//   seriesId: '0x303230390000',
-//   ilkId: FUSDC2306,
-//   baseId: USDC,
-// });
+LEVERS.set('FUSDC_2306', {
+  ...notionalLeverCommon_,
+  id: 'FUSDC_2306',
+  displayName: 'FUSDC_2306 <> USDC',
+  maturity: 1688137200,
+  fyTokenAddress: '0x667f185407C4CAb52aeb681f0006e4642d8091DF',
+  seriesId: '0x0032ff00028b',
+  ilkId: FUSDC2306,
+  baseId: USDC,
+});
 
+LEVERS.set('FDAI_2306', {
+  ...notionalLeverCommon_,
+  id: 'FDAI_2306',
+  displayName: 'FDAI_2306 <> DAI',
+  maturity: 1688137200,
+  fyTokenAddress: '0x9ca4D6fbE0Ba91d553e74805d2E2545b04AbEfEA',
+  seriesId: '0x0031ff00028b',
+  ilkId: FDAI2306,
+  baseId: DAI,
+});
+
+LEVERS.set('FETH_2306', {
+  ...notionalLeverCommon_,
+  id: 'FETH_2306',
+  displayName: 'FETH_2306 <> WETH',
+  maturity: 1688137200,
+  fyTokenAddress: '0x124c9F7E97235Fe3E35820f95D10aFfCe4bE9168',
+  seriesId: '0x0030ff00028b',
+  ilkId: FETH2306,
+  baseId: WETH,
+});
 
 // LEVERS.set('STRATEGY_01', {
 //   ...strategyLeverCommon_,
