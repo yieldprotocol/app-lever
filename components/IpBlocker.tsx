8f4615121286536ef78b179889510a0ef117eea2
brucedonovan <bruce.donovan@gmail.com>
2023-03-13 09:40:11 +0000

    ipBlocker complete

--- components/IpBlocker.tsx
+++ components/IpBlocker.tsx
@@ -1,26 +1,39 @@
-import React from 'react'
+import React from 'react';
 import useSWR from 'swr';
+import Modal from './common/Modal';
 
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const blockedCodes = ['US'];
 
-const fetcher = (url:string) => fetch(url).then((res) => res.json());
+const IpBlocker = (props: any) => {
+  const { data, error } = useSWR(
+    // `https://geo.ipify.org/api/v2/country?apiKey=${process.env.IPIFY_KEY}_disabled`,
+    `https://geo.ipify.org/api/v2/country?apiKey=${process.env.IPIFY_KEY}`,
+    fetcher
+  );
 
-const IpBlocker = (props: any) =>  {
-
-    const { data, error } = useSWR(
-        `https://geo.ipify.org/api/v2/country?apiKey=${process.env.IPIFY_KEY}_disabled`,
-        // `https://geo.ipify.org/api/v2/country?apiKey=${process.env.IPIFY_KEY}`,
-        fetcher
-      );
-      // if (error) return "An error has occurred.";
-  
-        data && console.log( 'ip address: ', data.ip, ' - location: ', data.location.country  )
-
-      return (
-        <div>
-            {props.children}    
-        </div>
-      );
-}
+  return (
+    <>
+      { (error || data?.location === undefined) && (
+        <Modal isOpen={true} setIsOpen={() => null}>
+          <div className="space-y-4">
+            <div className="text-lg text-white">The use of this dApp is restricted in some regions.</div>
+            <div className="text-xs text-white">
+              We could not definatively determine your location. Please turn off any VPNs or location mocking deivces.
+              If you are still experiencing issues, please contact support.
+            </div>
+          </div>
+        </Modal>
+      )}
+      {data?.location && blockedCodes.includes(data.location.country ) && (
+        <Modal isOpen={true} setIsOpen={() => null}>
+          <div className="text-lg text-white">Sorry, this dApp is not available in your country yet.</div>
+        </Modal>
+      )}
+      {data?.location && !blockedCodes.includes(data.location.country) && <div>{props.children}</div>}
+    </>
+  );
+};
 
 export default IpBlocker;
 
